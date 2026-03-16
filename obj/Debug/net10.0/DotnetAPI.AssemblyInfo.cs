0108657a854dea8df70694ae2f40d5e477f1addd
Yevhen Diadechko <eugeniyid@gmail.com>
2026-03-16 14:49:23 +0200

    added Auth controller with register and login user functionality

--- obj/Debug/net10.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad881ba7ed766b5aeb1f187487784f22d0dad322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e0d90ba9be212efff4de8bf22af99b8cd225f94")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
