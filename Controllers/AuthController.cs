1f25edc17f4ead87d83e8f40ea3210408d630721
Yevhen Diadechko <eugeniyid@gmail.com>
2026-03-25 07:20:32 +0200

    reworked register endpoint of auth controller in order to use stored procedures

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,5 @@
 using System.Data;
+using System.Globalization;
 using System.Security.Cryptography;
 using DotnetAPI.Data;
 using DotnetAPI.Dtos;
@@ -29,6 +30,7 @@ namespace DotnetAPI.Controllers
             if (userForRegistration.Password == userForRegistration.PasswordConfirm)
             {
                 string sqlCheckUserExists = "SELECT Email FROM TutorialAppSchema.Auth WHERE Email = '" + userForRegistration.Email + "'";
+                
                 IEnumerable<string> existingUsers = _dapper.LoadData<string>(sqlCheckUserExists);
                 if (existingUsers.Count() == 0)
                 {
@@ -40,24 +42,37 @@ namespace DotnetAPI.Controllers
 
                     byte[] passwordHash = _authHelper.GetPasswordHash(userForRegistration.Password, passwordSalt);
 
-                    string sqlAddAuth = "INSERT INTO TutorialAppSchema.Auth (Email, PasswordHash, PasswordSalt) VALUES ('" + userForRegistration.Email + "', @PasswordHash, @PasswordSalt)";
+                    string sqlAddAuth = @"EXEC TutorialAppSchema.spRegistration_Upsert 
+                            @Email = @EmailParam, 
+                            @PasswordHash = @PasswordHashParam, 
+                            @PasswordSalt = @PasswordSaltParam";
 
                     List<SqlParameter> sqlParameters = new List<SqlParameter>();
 
-                    SqlParameter passwordSaltParameter = new SqlParameter("@PasswordSalt", SqlDbType.VarBinary);
-                    passwordSaltParameter.Value = passwordSalt;
+                    SqlParameter emailParameter = new SqlParameter("@EmailParam", SqlDbType.VarChar);
+                    emailParameter.Value = userForRegistration.Email;
+                    sqlParameters.Add(emailParameter);
 
-                    SqlParameter passwordHashParameter = new SqlParameter("@PasswordHash", SqlDbType.VarBinary);
+                    SqlParameter passwordHashParameter = new SqlParameter("@PasswordHashParam", SqlDbType.VarBinary);
                     passwordHashParameter.Value = passwordHash;
-
-                    sqlParameters.Add(passwordSaltParameter);
                     sqlParameters.Add(passwordHashParameter);
+
+                    SqlParameter passwordSaltParameter = new SqlParameter("@PasswordSaltParam", SqlDbType.VarBinary);
+                    passwordSaltParameter.Value = passwordSalt;
+                    sqlParameters.Add(passwordSaltParameter);
 
                     if (_dapper.ExecuteSqlWithParameters(sqlAddAuth, sqlParameters))
                     {
 
-                        string sqlAddUser = @"INSERT INTO TutorialAppSchema.Users (FirstName, LastName, Email, Gender, Active)
-                            VALUES ('" + userForRegistration.FirstName + @"', '" + userForRegistration.LastName + @"', '" + userForRegistration.Email + @"', '" + userForRegistration.Gender + @"', 1)";
+                        string sqlAddUser = @"EXEC TutorialAppSchema.spUser_Upsert
+                                @FirstName = '" + userForRegistration.FirstName + @"',
+                                @LastName = '" + userForRegistration.LastName + @"',
+                                @Email = '" + userForRegistration.Email + @"',
+                                @Gender = '" + userForRegistration.Gender + @"',
+                                @Active = 1" + @",
+                                @JobTitle = '" + userForRegistration.JobTitle + @"',
+                                @Department = '" + userForRegistration.Department + @"',
+                                @Salary = " + userForRegistration.Salary.ToString(CultureInfo.InvariantCulture);
 
                         if (_dapper.ExecuteSql(sqlAddUser))
                         {
